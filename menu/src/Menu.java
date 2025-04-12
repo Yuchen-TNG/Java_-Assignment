327b38399e1655296ef9844f41f2d76e87e485dd
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-04-13 00:42:01 +0800

    a

---
+++ menu/src/Menu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Menu {
+    public static void main(String[] args) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Select Your Movie");
+        
+
+
+
+
+
+
+
+
+        cin.close();
+    }
+}
---
+++ menu/src/Movie.java
@@ -0,0 +1,22 @@
+import java.util.jar.Attributes.Name;   
+
+public class Movie {
+
+    public Movie(){
+        String[] Name= new String[20];
+        String[] ID = new String[20];
+        String[] Type = new String[20];
+        String[] Language= new String[20];  
+    }
+
+    public void setMovie(String name,String id,String type,String language){
+        
+        if(Name.length);
+
+        Name[0]=name;
+        ID[0]=id;
+        Type[0]=type;
+        Language[0]=language;
+    } 
+    
+}
