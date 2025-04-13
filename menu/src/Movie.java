f8d6cec02296a3d09f2a1c0fffa09ee060841d14
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-04-13 22:23:25 +0800

    Update Movie.java

--- menu/src/Movie.java
+++ menu/src/Movie.java
@@ -27,57 +27,63 @@ public class Movie {
         int addMovie = cin.nextInt();
         cin.nextLine();
         if (addMovie >= 1) {
-            for (int i = 0; i < addMovie; i++) {
-                System.out.print("Movie Name : ");
-                String MovieName = cin.nextLine();
-                Name.add(MovieName);
 
+            System.out.print("Movie Name : ");
+            String MovieName = cin.nextLine();
+            Name.add(MovieName);
+            boolean bool = true;
+            do {
                 System.out.print("Movie ID : ");
                 String MovieID = cin.nextLine();
-                if (MovieID.length()<4) {
-                    
-                } 
-                for(char c:MovieID.toCharArray()){
-
-                 }
-
-                 ID.add(MovieID);
-                System.out.print("Movie Type : ");
-                String MovieType = cin.nextLine();
-                Type.add(MovieType);
-
-                System.out.print("Movie Language : ");
-                String MovieLan = cin.nextLine();
-                Language.add(MovieLan);
-
-                System.out.println("\n\nYour Movie has been add");
-                System.out.println("Movie Name= "+MovieName);
-                System.out.println("Movie Name= "+MovieID);
-                System.out.println("Movie Name= "+MovieType);
-                System.out.println("Movie Name= "+MovieLan);
-            }
-        } else
+                if (MovieID.length() < 4) {
+                    for (int i = 1; 1 < 3;) {
+                        if (!Character.isDigit(MovieID.charAt(i))) {
+                            System.out.print("Your Movie ID is wrong format");
+                            bool = false;
+                        } else {
+                            ID.add(MovieID);
+                        }
+                    }
+                } else {
+                    System.out.print("Your Movie ID is wrong format");
+                    bool = false;
+                }
+            } while (bool == false);
+        } else {
             return;
+        }
+
+        System.out.print("Movie Type : ");
+        String MovieType = cin.nextLine();
+        Type.add(MovieType);
+
+        System.out.print("Movie Language : ");
+        String MovieLan = cin.nextLine();
+        Language.add(MovieLan);
+
+        System.out.println("\n\nYour Movie has been add");
+        System.out.println("Movie Name= " + MovieName);
+        System.out.println("Movie Name= " + MovieID);
+        System.out.println("Movie Name= " + MovieType);
+        System.out.println("Movie Name= " + MovieLan);
     }
 
-    public void clearMovie(String id){
+    public void clearMovie(String id) {
         System.out.println("Delete Movie List");
         System.out.print("Movie ID delete: ");
-        id=cin.nextLine();
-        int index=ID.indexOf(id);
-        if (index!=-1) {
+        id = cin.nextLine();
+        int index = ID.indexOf(id);
+        if (index != -1) {
             Name.remove(index);
             ID.remove(index);
             Type.remove(index);
             Language.remove(index);
-            System.out.println("Movie"+Name.get(index)+"have been deleted");
+            System.out.println("Movie" + Name.get(index) + "have been deleted");
             return;
-        }else{
+        } else {
             System.out.println("System can't find the Movie Details");
             return;
         }
-        
 
-        
     }
 }
