7a24503df67316651f1d97d374639edf502e719b
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-04-13 21:01:26 +0800

    a

--- menu/src/Menu.java
+++ menu/src/Menu.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) throws Exception {
         Scanner cin = new Scanner(System.in);
+        Movie movie = new Movie();
         System.out.println("Select Your Movie");
-        
-
+    
+        movie.setMovie();
 
 
 
