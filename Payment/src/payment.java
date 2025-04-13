e02cacb25b04fce1cb98324e440d23359099e196
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-04-13 23:54:29 +0800

    a

--- Payment/src/payment.java
+++ Payment/src/payment.java
@@ -3,10 +3,10 @@ import java.util.Date;
 public class payment {
     private String paymentID;
     private double paymentamount; // number of price of ticket
-    private String[] method; // touch n go, cash, credit card
+    private String method; // touch n go, cash, credit card
     private static double tax = 0.06;
     private double totalprice;// payment amount + (tax * paymentamount)
-    private boolean paymentsstatus; // true = success, false = failed
+    private boolean paymentstatus; // true = success, false = failed
     private String[] paymenthistory;
 
     //fakedata
@@ -18,18 +18,47 @@ public class payment {
     private String name= "Yuchen";
     private String email= "eason061221@gmail.com";
 
-    public String getpaymentID{
-
+    public String getpaymentID(){
+        return paymentID;
     }
 
-    
+    public double getpaymentamount(){
+        return paymentamount;
+    }
 
+    public String getmethod(){
+        return method;
+    }
 
+    public double gettotalprice(){
+        return totalprice;
+    }
 
+    public boolean getpaymentstatus(){
+        return paymentstatus;
+    }
 
-    
+    //set
 
+    public void setpaymentamount(double paymentamount){
+        this.paymentamount=paymentamount;
+    }
 
+    public void setpaymentID(String paymentID){
+        this.paymentID= paymentID;
+    }
+
+    public void setmethod(String method){
+        this.method=method;
+    }
+
+    public void settotalprice(double totalprice){
+        this.totalprice=totalprice;
+    }
+
+    public void setpaymentstatus(boolean paymentstatus){
+        this.paymentstatus=paymentstatus;
+    }
 
 
 }
