d77e31b06afc219dbeace1a11ad19e746b10c57a
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-04-16 08:12:02 +0800

    a

--- Payment/src/Mainpayment.java
+++ Payment/src/Mainpayment.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Mainpayment {
     public static void main(String[] args) throws Exception {
-        //ClassName objectName = new ClassName();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("how many ticket your buy?");
+        int ticketnum = input.nextInt();
+
+        System.out.println("Your ticketnum is" + ticketnum);
+
         
+        //ClassName objectName = new ClassName();
         payment details = new payment();
         //objects = 访问payment（class）里面的variable
         //new payments() 代表调用了payment的class
