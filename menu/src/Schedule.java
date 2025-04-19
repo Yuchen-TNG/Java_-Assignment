01e7d27b2b79eaec34e2e30caa1b7db3f4eb70f0
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-04-19 16:41:58 +0800

    Update Schedule.java

--- menu/src/Schedule.java
+++ menu/src/Schedule.java
@@ -2,12 +2,16 @@ import java.util.ArrayList;
 
 public class Schedule extends Movie {
     
-    ArrayList<String> Time = new ArrayList<>();
-    ArrayList<String> Date = new ArrayList<>();
-    ArrayList<String> dura = new ArrayList<>();
+    ArrayList<String> time = new ArrayList<>();
+    ArrayList<String> date = new ArrayList<>();
+    ArrayList<String> duration = new ArrayList<>();
     
     public Schedule(){
     }
 
-    public void addSchedule()
+    public void addSchedule(String time, String date, String duration){
+        this.time.add(time);
+        this.date.add(date);
+        this.duration.add(duration);
+    }
 }
