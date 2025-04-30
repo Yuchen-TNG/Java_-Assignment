99b62e42fd8b57cc9ae461c0d8fa18a3ff84d499
Leonchang06 <leoncrh-wm24@student.tarc.edu.my>
2025-04-30 19:26:50 +0800

    Schedule Done

--- Movie/src/Schedule.java
+++ Movie/src/Schedule.java
@@ -10,22 +10,22 @@ public class Schedule {
     ArrayList<String> movieID = new ArrayList<>();
     ArrayList<String> time = new ArrayList<>();
     ArrayList<String> date = new ArrayList<>();
-    ArrayList<String> duration = new ArrayList<>();
+    ArrayList<Double> duration = new ArrayList<>();
     DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
     Scanner cin = new Scanner(System.in); // 加上Scanner
 
     public Schedule() {
-        addSchedule("S001", "M001", "11:00", "2025-01-28", "2 hours");
-        addSchedule("S002", "M001", "12:00", "2025-02-27", "2 hours");
-        addSchedule("S003", "M001", "13:00", "2025-01-21", "2 hours");
-        addSchedule("S004", "M001", "14:00", "2025-02-01", "2 hours");
-        addSchedule("S005", "M002", "15:00", "2025-03-28", "1.5 hours");
-        addSchedule("S006", "M002", "13:00", "2025-04-28", "2 hours");
-        addSchedule("S007", "M003", "15:00", "2025-05-28", "1.5 hours");
+        addSchedule("S001", "M001", "11:00", "2025-01-28", 2);
+        addSchedule("S002", "M001", "12:00", "2025-02-27", 2);
+        addSchedule("S003", "M001", "13:00", "2025-01-21", 2);
+        addSchedule("S004", "M001", "14:00", "2025-02-01", 2);
+        addSchedule("S005", "M002", "15:00", "2025-03-28", 1.5);
+        addSchedule("S006", "M002", "13:00", "2025-04-28", 2);
+        addSchedule("S007", "M003", "15:00", "2025-05-28", 1.5);
     }
 
-    public void addSchedule(String scheduleId, String movieID, String time, String date, String duration) {
+    public void addSchedule(String scheduleId, String movieID, String time, String date, double duration) {
         this.scheduleId.add(scheduleId);
         this.movieID.add(movieID);
         this.time.add(time);
@@ -132,61 +132,138 @@ public class Schedule {
 
     public void setSchedule() {
         int selection;
-        String date;
-        String time;
+        int choice;
+        String date = "";
+        String time = "";
+        String movieID = "";
+        String scheduleID = "";
+        double duration = 0.0;
 
         showSchedule();
-        System.out.print("Selection: ");
-        selection = cin.nextInt();
-        System.out.print("ScheduleId: ");
-        String scheduleID = cin.next();
-        System.out.print("Movie ID: ");
-        String movieID = cin.next();
         while (true) {
-            System.out.print("Date: ");
-            date = cin.next();
-            if (date.matches("\\d{4}-\\d{2}-\\d{2}")) {
-                break;
-            } else {
-                System.out.println("invalid please follow the format(yyyy-mm-dd)");
-            }
-        }
-        while (true) {
-            System.out.print("Time: ");
-            time = cin.next();
-            if (time.matches("\\d{2}:\\d{2}")) {
-                int hour = Integer.parseInt(time.split(":")[0]);
-                int minute = Integer.parseInt(time.split(":")[1]);
-                if (hour < 24 && minute < 60) {
+            System.out.print("Selection: ");
+            if (cin.hasNextInt()) {
+                selection = cin.nextInt();
+                if (selection <= scheduleId.size() && selection > 0) {
                     break;
                 } else {
-                    System.out.println("Invalid please enter the time correctly");
+                    System.out.println("Invalid, please input the range of schedule lists");
                 }
             } else {
-                System.out.println("invalid please follow the format(00:00)");
+                System.out.println("Invalid, please enter the number");
+                cin.next();
             }
         }
-        System.out.print("Duration: ");
-        String duration = cin.next();
+        // =========================================================================================================================
+        do {
+            System.out.println("\n=====Edit Schedule=====");
+            System.out.println("1. Schedule Id");
+            System.out.println("2. Movie Id");
+            System.out.println("3. Date");
+            System.out.println("4. Time");
+            System.out.println("5. Duration");
+            System.out.println("6. Save");
+            System.out.println("=======================");
+            while (true) {
+                System.out.print("Choice: ");
+                if (cin.hasNextInt()) {
+                    choice = cin.nextInt();
+                    if (choice <= 6 && choice > 0) {
+                        break;
+                    } else {
+                        System.out.println("Invalid, please input the range of the choice ");
+                    }
+                } else {
+                    System.out.println("Invalid, please enter the number");
+                    cin.next();
+                }
+            }
 
-        this.scheduleId.set(selection - 1, scheduleID);
-        this.movieID.set(selection - 1, movieID);
-        this.date.set(selection - 1, date);
-        this.time.set(selection - 1, time);
-        this.duration.set(selection - 1, duration);
-
+            switch (choice) {
+                case 1:
+                    System.out.println("\n=====Edit Schedule Id=====");
+                    System.out.print("Schedule Id: ");
+                    scheduleID = cin.next();
+                    this.scheduleId.set(selection - 1, scheduleID);
+                    break;
+                case 2:
+                    System.out.println("\n=====Edit Movie Id=====");
+                    while (true) {
+                        System.out.print("Movie Id: ");
+                        movieID = cin.next();
+                        if (movieID.matches("^[M]\\d{3}")) {
+                            break;
+                        } else {
+                            System.out
+                                    .println(
+                                            "inavalid please follow the format : (M000),first character must be M and Uppercase");
+                        }
+                    }
+                    this.movieID.set(selection - 1, movieID);
+                    break;
+                case 3:
+                    System.out.println("\n=====Edit Date=====");
+                    while (true) {
+                        System.out.print("Date: ");
+                        date = cin.next();
+                        if (date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+                            break;
+                        } else {
+                            System.out.println("invalid please follow the format(yyyy-mm-dd)");
+                        }
+                    }
+                    this.date.set(selection - 1, date);
+                    break;
+                case 4:
+                    System.out.println("\n=====Edit Time=====");
+                    while (true) {
+                        System.out.print("Time: ");
+                        time = cin.next();
+                        if (time.matches("\\d{2}:\\d{2}")) {
+                            int hour = Integer.parseInt(time.split(":")[0]);
+                            int minute = Integer.parseInt(time.split(":")[1]);
+                            if (hour < 24 && minute < 60) {
+                                break;
+                            } else {
+                                System.out.println("Invalid please enter the time correctly");
+                            }
+                        } else {
+                            System.out.println("invalid please follow the format(00:00)");
+                        }
+                    }
+                    this.time.set(selection - 1, time);
+                    break;
+                case 5:
+                    System.out.println("\n=====Edit Duration=====");
+                    while (true) {
+                        System.out.print("Duration: ");
+                        if (cin.hasNextDouble() || cin.hasNextInt()) {
+                            duration = cin.nextDouble();
+                            break;
+                        } else {
+                            System.out.println("Invalid, please input the number");
+                            cin.next();
+                        }
+                    }
+                    this.duration.set(selection - 1, duration);
+                    break;
+                case 6:
+                    break;
+            }
+        } while (choice != 6);
         showSchedule();
 
     }
 
     public void showSchedule() {
-        System.out.println("====================Schedule Details====================");
+        System.out.println("\n========================SCHEDULE LISTS========================");
         System.out.printf("%-4s %-12s %-10s %-12s %-8s %-10s\n", "No", "ScheduleId", "Movie ID", "Date", "Time",
                 "Duration");
+        System.out.println("--------------------------------------------------------------");
 
         for (int i = 0; i < scheduleId.size(); i++) {
 
-            System.out.printf("%-4d %-12s %-10s %-12s %-8s %-10s\n",
+            System.out.printf("%-4d %-12s %-10s %-12s %-8s %-2s hours\n",
                     (i + 1),
                     this.scheduleId.get(i),
                     this.movieID.get(i),
@@ -194,6 +271,6 @@ public class Schedule {
                     this.time.get(i),
                     this.duration.get(i));
         }
-
+        System.out.println("==============================================================");
     }
 }
