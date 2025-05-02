00250a1365637ea02b7f5782983c08e60b1163a2
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-05-03 01:12:10 +0800

    a

--- Menu.java
+++ Menu.java
@@ -35,7 +35,7 @@ public class Menu {
                     System.out.println("Thank you for using Cinema Ticket System. Goodbye!");
                     break;
                 case 1:
-                    mv.choiceMovie();
+                    customer.Register();
                     break;
                 case 2:
                     customer.loginCustAcc();
